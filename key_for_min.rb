2f3295d7b4a1fe78e2be3c745f11baf51881a496
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-30 02:11:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.length != 0
     min_value = ''
+  if name_hash.length != 0
     name_hash.each do |key, value|
       if min_value > value
         min_value = key
       end
     end
-    
   else 
     return nil
   end
+  min_value
 end
