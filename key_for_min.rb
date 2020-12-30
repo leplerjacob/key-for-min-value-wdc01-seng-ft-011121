dd578c42290b56278b2e254b90a91b6648b4d4cf
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-30 02:22:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
     min_value = name_hash.values[0]
   if name_hash.length != 0
     name_hash.each do |key, value|
+      puts 
       if min_value > value
         min_value = key
       end
